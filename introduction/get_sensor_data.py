ddbc264ceaf0924f36c220a8cad2e6bf3a29569a
Freeqx <cziegler.offline@gmail.com>
2019-03-25 22:17:47 +0100

    Add files via upload

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 '''
 In this exercise you need to know how to get sensor data.
 
@@ -22,8 +22,8 @@ class MyAgent(SparkAgent):
         temperature = 0
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
-        temperature = self.perception.joint_temperature['HeadYaw']
-        angle = self.perception.joint['HeadYaw']
+        temperature = perception.joint_temperature['HeadYaw']
+        angle = perception.joint['HeadYaw']
 
         print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
         return super(MyAgent, self).think(perception)
@@ -31,37 +31,4 @@ class MyAgent(SparkAgent):
 if '__main__' == __name__:
     agent = MyAgent()
     agent.run()
-=======
-'''
-In this exercise you need to know how to get sensor data.
 
-* Task: get the current joint angle and temperature of joint HeadYaw
-
-* Hint: The current sensor data of robot are store in perception (class Perception in spark_agent.py)
-
-'''
-
-# add PYTHONPATH
-import os
-import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
-
-from spark_agent import SparkAgent
-
-
-class MyAgent(SparkAgent):
-    def think(self, perception):
-        angle = 0
-        temperature = 0
-        # YOUR CODE HERE
-        # get angle and temperature to current data of joint HeadYaw
-        temperature = self.perception.joint_temperature['HeadYaw']
-        angle = self.perception.joint['HeadYaw']
-
-        print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
-        return super(MyAgent, self).think(perception)
-
-if '__main__' == __name__:
-    agent = MyAgent()
-    agent.run()
->>>>>>> 17d2f0f7cf0721c7de22aa373d61b2b9362bf00b
